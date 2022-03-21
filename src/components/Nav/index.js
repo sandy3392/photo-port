e4873a5057e1097713c94fc16c2dd35c83e3aa07
Sandeep Vittala <applesandy33@gmail.com>
2022-03-21 12:27:40 -0400

    pushing test cases

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,18 +22,18 @@ function Nav() {
   return (
     <header>
         <h2>
-            <a href="/">
+            <a data-testid="link" href="/">
             <span role="img" aria-label="camera"> 📸</span> Oh Snap!
             </a>
         </h2>
         <nav>
             <ul className="flex-row">
                 <li className="mx-2">
-                    <a href="#about" >
+                    <a data-testid="about" href="#about" >
                     About me
                     </a>
                 </li>
-                <li>
+                <li className="mx-1">
                     <span>Contact</span>
                 </li>
                 {categories.map((category) => (
