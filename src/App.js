070c837f92320da587c1dd27cf35a62997868ab8
Sandeep Vittala <applesandy33@gmail.com>
2022-03-22 13:24:58 -0400

    pushing contact functionality

--- src/App.js
+++ src/App.js
@@ -17,19 +17,25 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>    
-      <Nav 
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      >
-      </Nav>
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory} ></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
